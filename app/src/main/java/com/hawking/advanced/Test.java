8f67fa7b7a04f222a91ce73f9cdeacb7a5ca7c02
hewking <cjh1095358798@gmail.com>
2021-01-24 17:38:02 +0800

    update

---
+++ app/src/main/java/com/hawking/advanced/Test.java
@@ -0,0 +1,17 @@
+package com.hawking.advanced;
+
+/**
+ * @Description: (用一句话描述该文件做什么)
+ * @Author: jianhao
+ * @Date: 1/9/21 5:50 PM
+ * @License: Copyright Since 2020 Hive Box Technology. All rights reserved.
+ * @Notice: This content is limited to the internal circulation of Hive Box,  and it is prohibited to leak or used for other commercial purposes.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("i am a test sample!");
+    }
+
+}
+
